a27b3fa0403c5be01467693de0875e7bf296b2d6
ollievargas <49657659+ollievargas@users.noreply.github.com>
2019-08-27 22:28:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -324,6 +324,33 @@ c.log();
 
 
 
+var arr = [
+    1, 
+    false,
+    {
+        name: 'Tony',
+        address: '111 main St.'
+    },
+    function(name) {
+        var greeting = 'Hello ';
+        console.log(greeting + name);
+    },
+    'hello'
+
+];
+
+console.log(arr);
+arr[3](arr[2].address);
+
+
+
+
+
+
+
+
+
+
 
 
 
